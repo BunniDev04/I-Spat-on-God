4c4703c802d56608e9ef5514131f9c3219440984
BunniDev <bunnidev04@gmail.com>
2021-10-17 08:46:16 -0700

    Installed error handler

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
-axm68k /o op+ /o os+ /o ow+ /o oz+ /o oaq+ /o osq+ /o omq+ /p /o ae- Main.asm, SMW.bin
-rompad.exe SMW.bin 255 0
+axm68k /o op+ /o os+ /o ow+ /o oz+ /o oaq+ /o osq+ /o omq+ /p /o ae- Main.asm, SMW.bin, output.sym, output.lst
+convsym output.lst SMW.bin -input asm68k_lst -inopt "/localSign=@ /localJoin=. /ignoreMacroDefs+ /ignoreMacroExp- /addMacrosAsOpcodes+" -a
 fixheadr.exe SMW.bin
 pause
